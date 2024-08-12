e7d1e266d94bc667d1c995d1d3f25d94fe5214a0
vahe <vahe@vahes-MacBook-Pro.local>
2024-08-12 23:00:13 +0400

    build

---
+++ _next/static/chunks/app/payment-success/page-c9c30e397c7e355a.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6685,1686,6304,4011,7447],{9971:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,2971,7023,1744],function(){return n(n.s=9971)}),_N_E=n.O()}]);
